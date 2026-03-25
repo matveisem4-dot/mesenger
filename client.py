715fe6e874dc3dd404723a085797fc8a2dce7f3a
matveisem4-dot <matveisem4@gmail.com>
2026-03-25 11:04:47 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,11 @@
-"""
-SecureMessenger Desktop Client
-Собирается в EXE через GitHub Actions
-"""
 import webview
 import sys
-import os
 
-SERVER = os.environ.get('SM_SERVER', 'http://localhost:5000')
-TITLE  = 'SecureMessenger'
+SERVER = "https://xxxxx.trycloudflare.com"
+
+if len(sys.argv) > 1:
+    SERVER = sys.argv[1]
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        SERVER = sys.argv[1]
-
-    window = webview.create_window(
-        TITLE, SERVER,
-        width=1100, height=750,
-        min_size=(400, 500),
-        confirm_close=True,
-        text_select=True
-    )
-    webview.start(debug=False)
+    webview.create_window('SecureMessenger', SERVER, width=1100, height=750)
+    webview.start()
